5eb705e2fdf54b1ea95697f6fc02bd0f8cc358ce
Shaik Rahimunnisa <rahishaik502@gmail.com>
2025-06-21 16:04:14 +0530

    Initial commit of Dockerized Flask Todo App

--- app.py
+++ app.py
@@ -45,6 +45,11 @@ def delete(todo_id):
     db.session.commit()
     return redirect(url_for("home"))
 
+
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
+
+
